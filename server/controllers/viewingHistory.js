f58b05b2a9c471dc7e09204bd5e4993ed2d18446
Woody See <woodyseesk@gmail.com>
2018-04-03 01:54:14 +0800

    api error handling

--- server/controllers/viewingHistory.js
+++ server/controllers/viewingHistory.js
@@ -4,16 +4,30 @@ exports.createViewingRecord = (req, res) => {
 	const viewingRecord = new ViewingRecord();
 	const data = req.body;
 	// console.log(data);
+	if (typeof data.ref === "undefined") {
+		res.status(403).send({status: 403, error: '403: Missing reference for adding viewing record, i.e. `ref`.'});
+		return;
+	}
+	if (typeof data.url === "undefined") {
+		res.status(403).send({status: 403, error: '403: Missing video URL, i.e. `url`.'});
+		return;
+	}
+	if (typeof data.played === "undefined") {
+		res.status(403).send({status: 403, error: "Missing video watched percentage, i.e. `played`."});
+		return;
+	}
 	viewingRecord.ref = data.id,
 	viewingRecord.url = data.url,
 	viewingRecord.played = data.played;
 	viewingRecord.updated = Date.now();
 	viewingRecord.save((err, viewingRecord) => {
 		if (err) {
-			res.status(406).send('Cannot create viewing record');
+			res.status(406).send({status: 406, error: err});
+			return;
 		}
 		// console.log(viewingRecord);
 		res.status(200).json(viewingRecord);
+		res.end();
 	});
 };
 
@@ -29,12 +43,15 @@ exports.showViewingHistory = (req, res, next) => {
 		sort(orderBy).
 		exec((error, viewingRecords) => {
 		if (error) {
-			return res.status(406).send(error);
+			res.status(406).json({status: 406, error: error});
+			return;
 		}
 		if (viewingRecords.length > 0) {
 			res.status(200).json(viewingRecords);
+			res.end();
 		} else {
-			res.status(204).send('No viewing history found');
+			res.status(204).send({status: 204, error: "No viewing record found."});
+			return;
 		}
 	});
 };
